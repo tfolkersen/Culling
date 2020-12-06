ca63542e57fc021b02add55cbcaa3690b7628ea1
Taylor Folkersen <folkerse@ualberta.ca>
2020-12-06 02:59:28 -0700

    comments

--- render/models.cpp
+++ render/models.cpp
@@ -2,9 +2,13 @@
 #include "utility.h"
 #include <fstream>
 
+Model cube; //small cube used to show where the light source is
+
+//empty Model constructor
 Model::Model() {
 }
 
+//copy Model constructor
 Model::Model(const Model &m) {
 	this->varr = m.varr;
 	this->posBuff = m.posBuff;
@@ -12,12 +16,11 @@ Model::Model(const Model &m) {
 	this->nVerts = m.nVerts;
 }
 
-
-
+//empty ModelCollection constructor
 ModelCollection::ModelCollection() {
 }
 
-
+//copy ModelCollection constructor
 ModelCollection::ModelCollection(const ModelCollection &m) {
 	this->main = m.main;
 	this->occluder = m.occluder;
@@ -28,52 +31,35 @@ ModelCollection::ModelCollection(const ModelCollection &m) {
 	this->marker = m.marker;
 }
 
-
-Model box;
-Model plant;
-Model cube;
-ModelCollection office;
-
-ModelCollection parseModelCollection(std::string mainFileName, GLfloat r, GLfloat g, GLfloat b, std::string occluderFileName, std::string boxFileName, std::string markerFileName) {
-	ModelCollection m;
-
-	m.main = parseObj(mainFileName, r, g, b);
-	m.occluder = parseObj(occluderFileName, 1.0f, 0.0f, 0.0f, m.occluderData);
-	m.box = parseObj(boxFileName, 1.0f, 1.0f, 0.0f, m.boxData);
-	m.boxCenter = modelDataCenter(m.boxData);
-	m.marker = parseObj(markerFileName, 0.0f, 1.0f, 1.0f);
-
-	return m;
-}
-
+//parse one obj file into a Model -- see header for details
 Model parseObj(std::string fileName, GLfloat r, GLfloat g, GLfloat b, std::vector<GLfloat> &posData){
 	posData.clear();
 
 	std::string line;
 	std::vector<std::string> tokens;
-	std::vector<std::vector<std::string>> subtokens;
+	std::vector<std::vector<std::string>> subtokens; //for numbers within each face element
 
-	std::vector<GLfloat> vertices;
-	std::vector<GLfloat> normals;
+	std::vector<GLfloat> vertices; //vertex locations specified by v elements
+	std::vector<GLfloat> normals; //normal data specified by vn elements
 
-	std::vector<GLfloat> normalData;
-	std::vector<GLfloat> colorData;
+	std::vector<GLfloat> normalData; //normal data to put in GL buffer
+	std::vector<GLfloat> colorData; //color data to put in GL buffer
 
-	std::fstream f(fileName);
+	std::fstream f(fileName); //open file and read all of the v and vn elements first
 	while (std::getline(f, line)) {
 		tokens = split(line, " ");
 		if (tokens.empty()) {
 			continue;
 		}
 
-		if (tokens[0] == "v") {
+		if (tokens[0] == "v") { //vertex element
 			GLfloat x = std::stof(tokens[1]);
 			GLfloat y = std::stof(tokens[2]);
 			GLfloat z = std::stof(tokens[3]);
 			vertices.push_back(x);
 			vertices.push_back(y);
 			vertices.push_back(z);
-		} else if (tokens[0] == "vn") {
+		} else if (tokens[0] == "vn") { //vertex normal element
 			GLfloat x = std::stof(tokens[1]);
 			GLfloat y = std::stof(tokens[2]);
 			GLfloat z = std::stof(tokens[3]);
@@ -82,28 +68,32 @@ Model parseObj(std::string fileName, GLfloat r, GLfloat g, GLfloat b, std::vecto
 			normals.push_back(z);
 		}
 	}
-	f.close();
+	f.close(); //close file
 
-	f = std::fstream(fileName);
+	f = std::fstream(fileName); //open file again and read all face elements
 	while (std::getline(f, line)) {
 		tokens = split(line, " ");
 		if (tokens.empty()) {
 			continue;
 		}
 
-		if (tokens[0] == "f") {
+		if (tokens[0] == "f") { //face element
 			subtokens.clear();
 
 			for (size_t j = 1; j < tokens.size(); j++) {
+				//for:
+				//f 1/2/3 7/4/8 ...
+				//subtokens are vectors of ["1", "2", "3"] and ["7", "4", "8"], etc...
 				subtokens.push_back(split(tokens[j], "/"));
 			}
 
-			for (size_t j = 0; j < subtokens.size() - 2; j++) {
-				for (size_t k = 0; k < 3; k++) {
-					int vi;
-					int vni;
+			//turn face into triangles (a triangle has vertices (0, j + 1, j + 2))
+			for (size_t j = 0; j < subtokens.size() - 2; j++) { //j + 1 is the first vertex to use that isn't vertex 0
+				for (size_t k = 0; k < 3; k++) { //vertex of this triangle
+					int vi; //vertex index
+					int vni; //vertex normal index
 
-					if (k == 0) {
+					if (k == 0) { //use first vertex of face
 						vi = std::stoi(subtokens[0][0]) - 1;
 						vni = std::stoi(subtokens[0][2]) - 1;
 					} else {
@@ -126,6 +116,7 @@ Model parseObj(std::string fileName, GLfloat r, GLfloat g, GLfloat b, std::vecto
 	}
 	f.close();
 
+	//Now load the model data into GL buffers
 	Model m;
 	glGenVertexArrays(1, &m.varr);
 	glBindVertexArray(m.varr);
@@ -144,9 +135,23 @@ Model parseObj(std::string fileName, GLfloat r, GLfloat g, GLfloat b, std::vecto
 
 	m.nVerts = posData.size() / 3;
 	return m;
-
 }
 
+
+//parse multiple obj files into a ModelCollection representing one object -- see header for details
+ModelCollection parseModelCollection(std::string mainFileName, GLfloat r, GLfloat g, GLfloat b, std::string occluderFileName, std::string boxFileName, std::string markerFileName) {
+	ModelCollection m;
+
+	m.main = parseObj(mainFileName, r, g, b);
+	m.occluder = parseObj(occluderFileName, 1.0f, 0.0f, 0.0f, m.occluderData); //save occluder data for depth buffer updates
+	m.box = parseObj(boxFileName, 1.0f, 1.0f, 0.0f, m.boxData); //save bounding box data for depth buffer tests
+	m.boxCenter = modelDataCenter(m.boxData); //get center of bounding box for sorting objects by depth later
+	m.marker = parseObj(markerFileName, 0.0f, 1.0f, 1.0f); //marker to show object in scene (to illustrate occlusion effect)
+
+	return m;
+}
+
+//return center of position data (format: x, y, z ...)
 glm::vec3 modelDataCenter(const std::vector<GLfloat> &data) {
 	GLfloat minX = data[0];
 	GLfloat maxX = minX;
@@ -169,5 +174,5 @@ glm::vec3 modelDataCenter(const std::vector<GLfloat> &data) {
 		it++;
 	}
 
-	return glm::vec3((minX + maxX) / 2.0f, (minY + maxY) / 2.0f, (minZ + maxZ) / 2.0f);
+	return glm::vec3((minX + maxX) / 2.0f, (minY + maxY) / 2.0f, (minZ + maxZ) / 2.0f); //mean coordinates
 }
--- render/models.h
+++ render/models.h
@@ -1,5 +1,12 @@
 #pragma once
 
+/*
+		models file
+
+		responsible for loading models from obj files and organizing
+		collections of meshes for objects
+*/
+
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 
@@ -7,46 +14,67 @@
 #include <string>
 
 
-
+//Model reprenting one mesh (one obj file)
 struct Model {
-	GLuint varr;
-	GLuint posBuff;
-	GLuint colBuff;
-	GLuint normBuff;
-	int nVerts;
+	GLuint varr; //GL vertex array ID
+	GLuint posBuff; //GL array_buffer id of position data
+	GLuint colBuff; //GL array_buffer id of color data
+	GLuint normBuff; //GL array_buffer ID of normal data
+	int nVerts; //number of vertices in model
 
 	Model();
 
-	Model(const Model& m);
+	Model(const Model& m); //copy constructor
 };
 
+extern Model cube; //used to show where the light source is
+
+/*		collection of meshes for one object
+
+	contains models for each rendering mode
+*/
 struct ModelCollection {
-	Model main;
+	Model main; //main mesh to render in GL
 
-	Model occluder;
-	std::vector<GLfloat> occluderData;
+	Model occluder; //occluder model to draw in GL
+	std::vector<GLfloat> occluderData; //raw occluder data (for rendering into depth buffer)
+	
+	Model box; //bounding box mesh to render in GL
+	std::vector<GLfloat> boxData; //raw box data (for depth test in depth buffer)
+	glm::vec3 boxCenter; //center point of bounding box (used for sorting scene objects)
 
-	Model box;
-	std::vector<GLfloat> boxData;
-	glm::vec3 boxCenter;
+	Model marker; //marker model to render in GL
 
-	Model marker;
-
-	glm::mat4 modelMatrix;
+	glm::mat4 modelMatrix; //model matrix for this object
 
 	ModelCollection();
 
-	ModelCollection(const ModelCollection& m);
+	ModelCollection(const ModelCollection& m); //copy constructor
 };
 
-extern Model box;
-extern Model plant;
-extern Model cube;
-extern ModelCollection office;
 
+/*		parse and obj file and return a model
 
+	fileName -- name of obj file
+	r, g, b are red, green, blue values to use for this model
+	posData is an optional argument that gets filled with the raw data (for getting raw occluder mesh/bounding box data)
+		this is also the buffer sent to the GL buffer
+*/
 Model parseObj(std::string fileName, GLfloat r, GLfloat g, GLfloat b, std::vector<GLfloat>& posData = std::vector<GLfloat>());
 
+/*		parse multiple obj files and store the models in a ModelCollection represnting one scene object
+
+
+	mainFileName -- file name of main mesh (the one actually seen in the scene)
+	r, g, b -- red, green, and blue values to use for the main mesh
+	occluderFileName -- file name of the occlusion mesh
+	boxFileName -- file name of the bounding box of the main mesh
+	markerFileName -- file name of the marker for the main mesh (this should be a very tall, thin pillar positioned somewhere near the center of the main mesh)
+
+
+*/
 ModelCollection parseModelCollection(std::string mainFileName, GLfloat r, GLfloat g, GLfloat b, std::string occluderFileName, std::string boxFileName, std::string markerFileName);
 
+/*	given coordinate data for a model, return the center point
+*/
 glm::vec3 modelDataCenter(const std::vector<GLfloat>& data);
